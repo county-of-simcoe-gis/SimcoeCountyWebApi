46866f7744df0a5e809d222522c03f40637a8e36
Al Proulx <alain.proulx@hotmail.com>
2020-01-10 15:44:59 -0500

    Insert Sample fixed

--- sqlScript.sql
+++ sqlScript.sql
@@ -86,8 +86,8 @@ CREATE TABLE public.tbl_search_layers (
 -- SAMPLE RECORDS FOR THE SEARCH
 -- INSERT INTO public.tbl_search_layers ("type",type_id,field_name,field_name_alias,muni_field_name,roll_number_field,run_schedule,priority,last_run,last_run_minutes,wfs_url) VALUES 
 -- ('Airport','sc_airport','name',NULL,'muni',NULL,'ALWAYS','10','2019-09-25 08:41:59.844',0.009483334,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Airport&outputFormat=application/json')
--- ,('Police Station','sc_policestation','name',NULL,'muni',NULL,'ALWAYS','5','2019-09-25 08:42:00.268',0.0069333334,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Police_Station&outputFormat=application/json')
--- ,('Museum And Archives','sc_museumandarchives','name',NULL,'muni',NULL,'ALWAYS','5','2019-09-25 08:42:00.55',0.00465,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Museum_and_Archives&outputFormat=application/json')
--- ,('Hospital','sc_hospital','name',NULL,'muni',NULL,'ALWAYS','5','2019-09-25 08:58:45.324',0.011983333,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Hospital&outputFormat=application/json')
+-- ,('Police Station','sc_policestation','Name',NULL,'Municipality',NULL,'ALWAYS','5','2019-09-25 08:42:00.268',0.0069333334,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Police_Station&outputFormat=application/json')
+-- ,('Museum And Archives','sc_museumandarchives','Name',NULL,'Municipality',NULL,'ALWAYS','5','2019-09-25 08:42:00.55',0.00465,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Museum_and_Archives&outputFormat=application/json')
+-- ,('Hospital','sc_hospital','Name',NULL,'Municipality',NULL,'ALWAYS','5','2019-09-25 08:58:45.324',0.011983333,'https://opengis.simcoe.ca/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeNames=simcoe:Hospital&outputFormat=application/json')
 -- ;
 
