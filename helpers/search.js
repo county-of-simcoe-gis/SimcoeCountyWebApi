d1c9e0028e5bb9bb5d1359ef0194336ed9766d84
Tom <58078313+reed-tom@users.noreply.github.com>
2021-06-24 14:51:34 -0400

    - switched address search to use values  - switched municipality compare to be case insensitive

--- helpers/search.js
+++ helpers/search.js
@@ -9,12 +9,20 @@ const useOSMSearch = searchConfig.useOSMSearch;
 
 const geocodeUrlTemplate = (limit, keywords) =>
   `https://maps.simcoe.ca/arcgis/rest/services/SimcoeUtilities/AddressLocator/GeocodeServer/findAddressCandidates?f=json&maxLocations=${limit}&outFields=House,StreetName,SufType,City&Street=${keywords}`;
-const osmUrlTemplateViewBox = (viewBox, limit, keywords) => `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&viewbox=${viewBox}&bounded=1&limit=${limit}&q=${keywords}`;
-const osmUrlTemplateNoViewBox = (limit, keywords) => `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&countrycodes=ca&limit=${limit}&q=${keywords}`;
+const osmUrlTemplateViewBox = (viewBox, limit, keywords) =>
+  `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&viewbox=${viewBox}&bounded=1&limit=${limit}&q=${keywords}`;
+const osmUrlTemplateNoViewBox = (limit, keywords) =>
+  `https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&countrycodes=ca&limit=${limit}&q=${keywords}`;
 
 module.exports = {
-  search: async function (keywords, type = undefined, muni = undefined, limit = 10, callback) {
-    try{
+  search: async function (
+    keywords,
+    type = undefined,
+    muni = undefined,
+    limit = 10,
+    callback
+  ) {
+    try {
       // MINIMUM 1 CHAR
       if (keywords.length < 2) {
         callback([]);
@@ -31,8 +39,14 @@ module.exports = {
         addresses = await this._searchAddress(keywords, muni, type, limit);
 
         // FALL BACK TO GEOCODE
-        if (useESRIGeocoder && addresses.length === 0 && (type === "Address" || type === undefined || type === "All")) {
-          const geocodeResult = await this._getJSON(geocodeUrlTemplate(limit, keywords));
+        if (
+          useESRIGeocoder &&
+          addresses.length === 0 &&
+          (type === "Address" || type === undefined || type === "All")
+        ) {
+          const geocodeResult = await this._getJSON(
+            geocodeUrlTemplate(limit, keywords)
+          );
           if (geocodeResult !== undefined) {
             const candidates = geocodeResult.candidates;
             if (candidates !== undefined) {
@@ -58,19 +72,32 @@ module.exports = {
       // FILL IN THE SEARCH WITH OTHERS
       if (allValues.length < limit) {
         const numRecordsToReturn = limit - allValues.length;
-        let nonAddresses = await this._searchNonAddress(keywords, type, muni, limit);
+        let nonAddresses = await this._searchNonAddress(
+          keywords,
+          type,
+          muni,
+          limit
+        );
         allValues.push(...nonAddresses);
       }
 
       // // FILL IN THE SEARCH WITH OSM, ONLY IF NO RESULTS
-      if ((useOSMSearch && allValues.length === 0) || type === "Open Street Map" || (allValues.length === 0 && type === "All")) {
+      if (
+        (useOSMSearch && allValues.length === 0) ||
+        type === "Open Street Map" ||
+        (allValues.length === 0 && type === "All")
+      ) {
         const numRecordsToReturn = limit - allValues.length;
-        let osmPlaces = await this._searchOsm(keywords, type, numRecordsToReturn);
+        let osmPlaces = await this._searchOsm(
+          keywords,
+          type,
+          numRecordsToReturn
+        );
         allValues.push(...osmPlaces);
       }
 
       callback(allValues);
-    }catch (e) {
+    } catch (e) {
       callback([]);
     }
   },
@@ -89,28 +116,35 @@ module.exports = {
     const pg = new postgres({ dbName: "tabular" });
     pg.selectAll(sql, (result) => {
       let types = [];
-      if (result.length >0 ){
+      if (result.length > 0) {
         result.forEach((type) => {
           types.push(type.type);
         });
-        
       }
       callback(types);
     });
   },
 
-  _searchAddress: async function (value, muni = undefined, type = undefined, limit = 10) {
+  _searchAddress: async function (
+    value,
+    muni = undefined,
+    type = undefined,
+    limit = 10
+  ) {
     const values = [value];
 
     if (type === "All" || type === "undefined") type = undefined;
 
     let sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike $1 || '%' and type = 'Address'`;
     if (muni !== undefined && muni !== "undefined") {
-      sql += " and municipality = '" + muni + "'";
+      sql += " and lower(municipality) = lower($2)";
+      values.push(muni);
     }
 
     if (type !== undefined && type !== "undefined") {
-      sql += " and type = '" + type + "'";
+      if (muni !== undefined && muni !== "undefined") sql += " and type = $3";
+      else sql += " and type = $2";
+      values.push(type);
     }
 
     sql += " limit " + limit + ";";
@@ -120,7 +154,12 @@ module.exports = {
     return pgResult;
   },
 
-  _searchNonAddress: async function (value, type = undefined, muni = undefined, limit = 10) {
+  _searchNonAddress: async function (
+    value,
+    type = undefined,
+    muni = undefined,
+    limit = 10
+  ) {
     if (type === "undefined" || type === "All") type = undefined;
     if (muni === "undefined") muni = undefined;
 
@@ -130,11 +169,11 @@ module.exports = {
       sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike $1 || '%' and type <> 'Address' order by priority limit $2;`;
     if (muni !== undefined && type === undefined) {
       values.push(muni);
-      sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike $1 || '%' and type <> 'Address' and municipality = $3 limit $2;`;
+      sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike $1 || '%' and type <> 'Address' and lower(municipality) = lower($3) limit $2;`;
     } else if (muni !== undefined && type !== undefined) {
       values.push(muni);
       values.push(type);
-      sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike '%' || $1 || '%' and type = $4 and municipality = $3 and type <> 'Address' limit $2;`;
+      sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike '%' || $1 || '%' and type = $4 and lower(municipality) = lower($3) and type <> 'Address' limit $2;`;
     } else if (muni === undefined && type !== undefined) {
       values.push(type);
       sql = `select name,type,municipality,location_id from public.tbl_search  where name ilike '%' || $1 || '%' and type = $3 and type <> 'Address' limit $2;`;
