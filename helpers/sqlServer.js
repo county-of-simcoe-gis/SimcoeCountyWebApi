8f1a7414c72e71155a176446dc3fbec84c5e76da
Tom <58078313+reed-tom@users.noreply.github.com>
2021-08-09 14:21:40 -0400

    fixed issue with getRadarImages

---
+++ helpers/sqlServer.js
@@ -0,0 +1,195 @@
+const sql = require("mssql");
+const config = require("../config.json");
+
+module.exports = class sqlServer {
+  constructor(opt) {
+    let conInfo = null;
+    if (opt.dbName === "tabular") conInfo = config.sqlServerConnectionTabular;
+    else if (opt.dbName === "weblive") conInfo = config.sqlServerConnectionWeblive;
+
+    const sqlConStringTemplate = (userName, password, server, db) => `mssql://${userName}:${password}@${server}/${db}`;
+    this.sqlConString = sqlConStringTemplate(conInfo.user, conInfo.password, conInfo.host, conInfo.database);
+
+    // promise style:
+    this.pool = new sql.ConnectionPool(this.sqlConString);
+    this.poolConnect = this.pool.connect();
+
+    this.pool.on("error", (err) => {
+      console.log("Error Connecting to Sql Server");
+    });
+  }
+  select(sqlString, callback) {
+    if (sqlString.includes("'")) sqlString.replace("''");
+    this.poolConnect.then((pool) => {
+      pool.request().query(sqlString, (err, result) => {
+        if (err !== null) console.log(err);
+
+        callback(result);
+        sql.close();
+      });
+    });
+  }
+  // RETURN FIRST RECORD
+  selectFirst(sqlString, callback) {
+    try {
+      this.poolConnect.then((pool) => {
+        const request = pool.request();
+        request.query(sqlString, (err, res) => {
+          if (err !== null) console.log(err);
+          if (!res) {
+            callback({ error: "Query returned ZERO records." });
+          } else {
+            callback(res.recordset[0]);
+          }
+          sql.close();
+        });
+      });
+    } catch (err) {
+      callback({ error: "Query failed!" });
+    }
+  }
+  // RETURN MULTIPLE RECORDS
+  selectAll(sqlString, callback) {
+    try {
+      this.poolConnect.then((pool) => {
+        const request = pool.request();
+        request.query(sqlString, (err, res) => {
+          if (err !== null) console.log(err);
+          if (!res) {
+            callback({ error: "Query returned ZERO records." });
+          } else {
+            callback(res.recordset);
+          }
+          sql.close();
+        });
+      });
+    } catch (err) {
+      callback({ error: "Query failed!" });
+    }
+  }
+  // RETURN MULTIPLE RECORDS
+  selectAllWithValues(sqlString, values, callback) {
+    try {
+      this.poolConnect.then((pool) => {
+        const request = pool.request();
+        values.forEach((item) => {
+          request.input(item.name, this.getSqlType(item.type, item.typeOpts), item.value);
+        });
+        request.query(sqlString, (err, res) => {
+          if (err !== null) console.log(err);
+          if (!res) {
+            callback({ error: "Query returned ZERO records." });
+          } else {
+            callback(res.recordset);
+          }
+        });
+      });
+    } catch (err) {
+      callback({ error: "Query failed!" });
+    } finally {
+      sql.close();
+    }
+  }
+  // RETURN FIRST RECORD
+  selectFirstWithValues(sqlString, values, callback) {
+    try {
+      this.poolConnect.then((pool) => {
+        const request = pool.request();
+        values.forEach((item) => {
+          request.input(item.name, this.getSqlType(item.type, item.typeOpts), item.value);
+        });
+        request.query(sqlString, (err, res) => {
+          if (err !== null) console.log(err);
+          if (!res) {
+            callback({ error: "Query returned ZERO records." });
+          } else {
+            callback(res.recordset);
+          }
+        });
+      });
+    } catch (err) {
+      callback({ error: "Query failed!" });
+    } finally {
+      sql.close();
+    }
+  }
+  getSqlType(type, opts = undefined) {
+    let precision = 0;
+    let scale = 0;
+    let length = 0;
+    if (opts) {
+      if (opts.precision) precision = opts.precision;
+      if (opts.scale) scale = opts.scale;
+      if (opts.length) precision = opts.length;
+    }
+
+    switch (type) {
+      case "Bit":
+        return sql.Bit;
+      case "BigInt":
+        return sql.BigInt;
+      case "Decimal":
+        return sql.Decimal(precision, scale);
+      case "Float":
+        return sql.Float;
+      case "Int":
+        return sql.Int;
+      case "Money":
+        return sql.Money;
+      case "Numeric":
+        return sql.Numeric(precision, scale);
+      case "SmallInt":
+        return sql.SmallInt;
+      case "SmallMoney":
+        return sql.SmallMoney;
+      case "Real":
+        return sql.Real;
+      case "TinyInt":
+        return sql.TinyInt;
+      case "Char":
+        return sql.Char(length);
+      case "NChar":
+        return sql.NChar(length);
+      case "Text":
+        return sql.Text;
+      case "NText":
+        return sql.NText;
+      case "VarChar":
+        return sql.VarChar(length);
+      case "NVarChar":
+        return sql.NVarChar(length);
+      case "Xml":
+        return sql.Xml;
+      case "Time":
+        return sql.Time(scale);
+      case "Date":
+        return sql.Date;
+      case "DateTime":
+        return sql.DateTime;
+      case "DateTime2":
+        return sql.DateTime2(scale);
+      case "DateTimeOffset":
+        return sql.DateTimeOffset(scale);
+      case "SmallDateTime":
+        return sql.SmallDateTime;
+      case "UniqueIdentifier":
+        return sql.UniqueIdentifier;
+      case "Variant":
+        return sql.Variant;
+      case "Binary":
+        return sql.Binary;
+      case "VarBinary":
+        return sql.VarBinary(length);
+      case "Image":
+        return sql.Image;
+      case "UDT":
+        return sql.UDT;
+      case "Geography":
+        return sql.Geography;
+      case "Geometry":
+        return sql.Geometry;
+      default:
+        return sql.Text;
+    }
+  }
+};
--- helpers/weather.js
+++ helpers/weather.js
@@ -1,4 +1,4 @@
-const mssql = require("./mssql");
+const sqlServer = require("./sqlServer");
 const common = require("./common");
 const fetch = require("node-fetch");
 var xml2js = require("xml2js");
@@ -6,10 +6,10 @@ var parser = new xml2js.Parser();
 
 module.exports = {
   getRadarImages: function (fromDate, toDate, callback) {
-    const db = new mssql({ dbName: "tabular" });
+    const db = new SqlServer({ dbName: "tabular" });
     const values = [
-      { name: "fromDate", type: "DateTime", value: fromDate },
-      { name: "toDate", type: "DateTime", value: toDate },
+      { name: "fromDate", type: "Date", value: fromDate },
+      { name: "toDate", type: "Date", value: toDate },
     ];
     const sqlQuery = `SELECT [RADAR_CODE],[RADAR_DESCRIPTION],[RADAR_DATE],[JS_MAPIMAGE],[FILE_NAME],datediff(minute,'2015-1-1',[RADAR_DATE]) as TIME_ID 
                       FROM dbo.tbl_Weather_Radar_Current_Images 
