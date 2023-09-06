31fdecfddc4aef013133a679d5b5efc8f194b6f7
Tom <58078313+reed-tom@users.noreply.github.com>
2023-09-06 13:57:23 -0400

    Feature/powerbi reports (#48)
    * switched reports to use power bi report
    
    * added endpoints to generate embedded powerbi reports
    
    * added default route to route builder
    
    * updated endpoint swagger description

--- helpers/reportGenerator.js
+++ helpers/reportGenerator.js
@@ -1,39 +1,21 @@
-const ssrs = require("./ssrs");
+// const ssrs = require("./ssrs");
+const PowerBI = require("./powerbi");
+
 const sqlServer = require("./sqlServer");
 const postgres = require("./postgres");
 
 const ss = new sqlServer({ dbName: "tabular" });
-const reports = new ssrs();
+// const reports = new ssrs();
+const reports = new PowerBI();
 
 module.exports = {
   async getPropertyReport(arn, callback) {
-    try {
-      const obj = JSON.parse(body);
-      const geoJSON = obj.geoJson;
-      const reportType = obj.reportType;
-      const reportFormat = obj.reportFormat;
+    var parameters = [{ Name: "ARN", Value: arn }];
 
-      const sql = "select string_agg(arn, '|') AS arns from weblive.sde.teranet_dapf where ST_Intersects(geom, ST_SetSRID(ST_GeomFromGeoJSON($1), 3857))";
-      var values = [geoJSON];
-      const pg = new postgres({ dbName: "weblive" });
-      pg.selectFirstWithValues(sql, values, (result) => {
-        if (result.arns === null) {
-          callback("Zero Records");
-        } else {
-          var parameters = [
-            { Name: "ARNS", Value: result.arns },
-            { Name: "UsePartnerData", Value: false },
-          ];
-
-          reports.runReport(reportType, parameters, "Mailing_Labels", reportFormat, (fn) => {
-            console.log("Saved report to: " + fn);
-            callback(fn);
-          });
-        }
-      });
-    } catch (error) {
-      callback(error);
-    }
+    reports.runReport("/PropertyReports/Public", parameters, "PropertyReport", "PDF", (fn) => {
+      console.log("Saved report to: " + fn);
+      callback(fn);
+    });
   },
   async getEconomicDevelopmentReportMLS(mls, callback) {
     var parameters = [{ Name: "MLSNUMBER", Value: mls }];
--- routes/public/reports/property.js
+++ routes/public/reports/property.js
@@ -1,5 +1,7 @@
 const propertyReport = require("../../../helpers/propertyReport");
 const common = require("../../../helpers/common");
+const reportGenerator = require("../../../helpers/reportGenerator");
+const fs = require("fs");
 
 module.exports = (baseRoute, middleWare, router) => {
   router.get(baseRoute + "/:arn", middleWare, (req, res, next) => {
@@ -64,6 +66,43 @@ module.exports = (baseRoute, middleWare, router) => {
       console.error(e.stack);
       res.status(500);
       res.send();
+      return next();
+    }
+  });
+  router.get(baseRoute + "/pdf/:arn", middleWare, (req, res, next) => {
+    /* 
+      #swagger.tags = ['Public/Reports']
+      #swagger.path = '/public/reports/property/{arn}'
+      #swagger.deprecated = false
+      #swagger.ignore = false
+      #swagger.summary = 'Retrieve property report'
+       #swagger.parameters['arn'] = {
+          in: 'path',
+          description: 'ARN',
+          required: true,
+          type: 'string'
+      }
+       
+    */
+    try {
+      if (!common.isHostAllowed(req, res)) return;
+
+      reportGenerator.getPropertyReport(req.params.arn, (result) => {
+        const fileExists = fs.existsSync(result);
+        if (!fileExists) {
+          res.send(result);
+        } else {
+          var file = fs.createReadStream(result);
+          var stat = fs.statSync(result);
+          res.setHeader("Content-Length", stat.size);
+          res.setHeader("Content-Type", "application/pdf");
+          res.setHeader("Content-Disposition", "attachment; filename=EconomicDevelopmentReport.pdf");
+          file.pipe(res);
+        }
+      });
+    } catch (e) {
+      console.error(e.stack);
+      res.status(500).send();
       return next();
     }
   });
